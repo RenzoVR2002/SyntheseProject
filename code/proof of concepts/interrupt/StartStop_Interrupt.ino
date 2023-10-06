e43499647d074b1c8f7263f76c158351783f243f
RenzoVR2002 <146451808+RenzoVR2002@users.noreply.github.com>
2023-10-06 15:45:09 +0200

    Update StartStop_Interrupt.ino

--- code/proof of concepts/interrupt/StartStop_Interrupt.ino
+++ code/proof of concepts/interrupt/StartStop_Interrupt.ino
@@ -1,30 +1,14 @@
-const int Schakelaar = 2; 
-int VorigeStatus = LOW; 
-unsigned long lastDebounceTime = 0; 
-unsigned long debounceDelay = 50; 
-
-void setup() {
-  pinMode(Schakelaar, INPUT);
-  digitalWrite(Schakelaar, HIGH);
-  Serial.begin(9600);
+const int SchakelaarPin = 2; 
+volatile bool SchakelaarState = LOW; 
+void setup() { 
+  pinMode(SchakelaarPin, INPUT); 
+attachInterrupt(digitalPinToInterrupt(SchakelaarPin), SchakelaarISR, CHANGE);
+Serial.begin(9600); 
 }
 
-void loop() {
-  int Status = digitalRead(Schakelaar);
-
-  if (Status != VorigeStatus) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (Status != VorigeStatus) {
-      VorigeStatus = Status;
-      
-      if (Status == HIGH) {
-        Serial.println("Schakelaar is aan!");
-      } else {
-        Serial.println("Schakelaar is uit!");
-      }
-    }
-  }
+void loop() { }
+void SchakelaarISR()
+{ SchakelaarState = digitalRead(SchakelaarPin); 
+Serial.print("Drukknop ingedrukt: ");
+ Serial.println(SchakelaarState); 
 }
